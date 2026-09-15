9f7e7a78f777d89b6920f3e55b79443f4fee86cb
igarukas4 <igarukas4@users.noreply.github.com>
2026-09-15 18:44:55 +0800

    Stop su parsing Uvicorn options

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,4 +6,4 @@ if [ -n "${DATABASE_PASSWORD_FILE:-}" ] && [ -r "$DATABASE_PASSWORD_FILE" ]; the
   export DATABASE_PASSWORD_FILE=/tmp/postgres_password
 fi
 
-exec su -s /bin/sh app -c 'exec "$@"' sh "$@"
+exec su -s /bin/sh -c 'exec "$@"' -- app sh "$@"
